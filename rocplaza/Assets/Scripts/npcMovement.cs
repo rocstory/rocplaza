b2347339433fb9061d7453d709d40b174bdfb51a
Malik Roc <malikr@Maliks-MBP-35.home>
2020-10-18 20:06:40 -0400

    random npc customization

--- rocplaza/Assets/Scripts/npcMovement.cs
+++ rocplaza/Assets/Scripts/npcMovement.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class npcMovement : MonoBehaviour
 {
+    public float speed;
+    public bool moveRight;
+
+    SpriteRenderer spriteRender;
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        spriteRender = GetComponent<SpriteRenderer>(); 
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.05f, 0, 0);
+        float xScale = transform.localScale.x;
+        float yScale = transform.localScale.y;
+        float zScale = transform.localScale.z;
+
+        if (moveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            float negative = -Mathf.Abs(xScale);
+            transform.localScale = new Vector3(negative, yScale, zScale);
+
+            //transform.localScale = new Vector2(-xScale, yScale);
+
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            float positive = Mathf.Abs(xScale);
+            transform.localScale = new Vector3(positive, yScale, zScale);
+            
+            //transform.localScale = new Vector2(xScale, yScale);
+        }
     }
 
     void OnCollisionEnter2D (Collision2D col)
--- rocplaza/Assets/Scripts/npcSpawner.cs
+++ rocplaza/Assets/Scripts/npcSpawner.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class npcSpawner : MonoBehaviour
 {
     public GameObject npc;
     public float spawnRate = 2f; // in seconds
-
+    public List<Sprite> headSpr = new List<Sprite>();
+    public List<Sprite> legsSpr = new List<Sprite>();
+    public List<Sprite> upperBodySpr = new List<Sprite>();
+    public List<Sprite> skinSpr = new List<Sprite>();
     Vector2 whereToSpawn;
     float nextSpawn = 0.0f;
-
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +26,49 @@ public class npcSpawner : MonoBehaviour
         if (Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            //randX = Random.Range(-8.4f, 8.4);
             whereToSpawn = new Vector2(transform.position.x, transform.position.y);
+            generateRandomNPC();
             Instantiate(npc, whereToSpawn, Quaternion.identity);
         }
+
+    }
+    
+    void generateRandomNPC()
+    {
+        foreach(Transform child in npc.transform)
+        {
+            SpriteRenderer childSprite = child.GetComponent<SpriteRenderer>();
+            int spriteIndex = 0;
+
+            switch(child.tag)
+            {
+                case "npcUpperBody":
+                    spriteIndex = getRandomSprite(upperBodySpr);
+                    childSprite.sprite = upperBodySpr[spriteIndex]; 
+                    break;
+                case "npcLegs":
+                    spriteIndex = getRandomSprite(legsSpr);
+                    childSprite.sprite = legsSpr[spriteIndex]; 
+                    break;
+                case "npcHead":
+                    spriteIndex = getRandomSprite(headSpr);
+                    childSprite.sprite = headSpr[spriteIndex]; 
+                    break;
+                case "npcSkin":
+                    spriteIndex = getRandomSprite(skinSpr);
+                    childSprite.sprite = skinSpr[spriteIndex]; 
+                    break;
+                default:
+                    spriteIndex = getRandomSprite(skinSpr);
+                    childSprite.sprite = skinSpr[spriteIndex];
+                    break;
+            }
+        } // foreach
+
+    }
+
+    int getRandomSprite(List<Sprite> spriteSet)
+    {
+        return Random.Range(0, spriteSet.Count - 1);
     }
 }
